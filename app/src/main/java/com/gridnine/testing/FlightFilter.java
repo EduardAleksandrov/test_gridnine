fcc8e6e0f978fb4293368e3d10a813ece615a551
Eduard Aleksandrov <eduard.ale@list.ru>
2026-03-03 18:36:15 +0300

    fix

--- app/src/main/java/com/gridnine/testing/FlightFilter.java
+++ app/src/main/java/com/gridnine/testing/FlightFilter.java
@@ -5,6 +5,7 @@ import java.util.List;
 /**
  * Интерфейс для реализации различных правил фильтрации.
  */
+@FunctionalInterface
 public interface FlightFilter {
     List<Flight> filter(List<Flight> flights);
 }
